d58e7fe5ebf994d06685581d8b937acf6de95867
Niil <91337648+neeeal@users.noreply.github.com>
2024-01-29 12:31:17 +0800

    refactor: css removed auto and rearranged navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -12,13 +12,7 @@
             <router-link :to="{name:'home'}" class="nav-link" aria-current="page">Home</router-link>
           </li>
         </ul>
-        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link :to="{name:'login'}" class="nav-link" aria-current="page">Login</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link :to="{name:'register'}" class="nav-link" aria-current="page">Register</router-link>
-          </li>
+        <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               User
@@ -28,6 +22,12 @@
               <li><hr class="dropdown-divider"></li>
               <li><button class="dropdown-item btn btn-danger">Logout</button></li>
             </ul>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name:'login'}" class="nav-link" aria-current="page">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name:'register'}" class="nav-link" aria-current="page">Register</router-link>
           </li>
         </ul>
       </div>
